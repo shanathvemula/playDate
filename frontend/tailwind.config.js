78a588a4376158a6f4a556b4854488340ebe3079
shanathvemula <shanathvemula@panfx.com>
2024-10-07 14:18:57 +0530

    Updated the Ip configration

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,22 +4,8 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
-  server: {
-    host: '69.197.176.103',  // Allows external access from any IP (local network)
-    port: 3000,        // Port number where the app will run
-    open: true,
-    proxy: {
-      '/api': {
-        // target: 'http://localhost:8000/',
-        target: 'http://69.197.176.103:8000/',  // Example API proxy to another server
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),  // Optional path rewrite
-      },
-    },
-  },
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: "69.197.176.103", // e.g., '192.168.1.100'
+    port: 5173, // e.g., 3000
+    proxy: {
+      "/api": {
+        target: "http://69.197.176.103:8000/",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
 })
