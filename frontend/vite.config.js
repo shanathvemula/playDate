d3e3a4ee8339aa1247d7b3cdcb8b8926adedada6
VEMULA SHANATH KUMAR <65351126+shanathvemula@users.noreply.github.com>
2024-10-09 21:54:18 +0530

    Updated vite configuration

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: "69.197.176.103", // e.g., '192.168.1.100'
+    host: "localhost", // e.g., '192.168.1.100'
     port: 3000,
     proxy: {
       "/api": {
