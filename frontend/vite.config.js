481a93556eebdb62c86ebdd89f5fc55b7d6be609
VEMULA SHANATH KUMAR <65351126+shanathvemula@users.noreply.github.com>
2024-10-09 21:50:11 +0530

    Updated vite configuration

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: "69.197.176.103", // e.g., '192.168.1.100'
-    port: 5173, // e.g., 3000
+    port: 3000,
     proxy: {
       "/api": {
         target: "http://69.197.176.103:8000/",
