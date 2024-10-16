2b629eb67f5176ddba96577378dcf1b2bd41b383
VEMULA SHANATH KUMAR <65351126+shanathvemula@users.noreply.github.com>
2024-10-16 18:35:03 +0530

    Updated

--- playDate/settings.py
+++ playDate/settings.py
@@ -304,11 +304,11 @@ SPECTACULAR_SETTINGS = {
 # )
 
 # Email settings
-EMAIL_HOST = "smtp.gmail.com" # 'mail.phantomfx.org'
-EMAIL_PORT = 465
-EMAIL_USE_TLS = False
-EMAIL_HOST_USER = "shanath1213@gmail.com" # 'shanathvemula@phantomfx.org'
-EMAIL_HOST_PASSWORD = "vddu hjhd qagh xchr" # 'Sh@n@th321'
+EMAIL_HOST = os.getenv("EMAIL_HOST")
+EMAIL_PORT = os.getenv("EMAIL_PORT")
+EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS")
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
