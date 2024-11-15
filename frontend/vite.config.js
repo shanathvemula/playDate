a754f6fc545e933cccfa5cfb901b29c58c25f951
shanathvemula <shanathvemula@panfx.com>
2024-11-15 16:37:38 +0530

    Updated the Dockerfile and nginx.conf file

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,12 +7,12 @@ export default defineConfig({
   server: {
     host: "localhost", // '157.173.195.249', // 
     port: 3000,
-    // proxy: {
-    //   "/api": {
-    //     target: "http://157.173.195.249:8000/", //  "http://127.0.0.1:8000/", // 
-    //     changeOrigin: true,
-    //     rewrite: (path) => path.replace(/^\/api/, ""),
-    //   },
-    // },
+    proxy: {
+      "/api": {
+        target: "http://157.173.195.249:8000/", //  "http://127.0.0.1:8000/", // 
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
   },
 })
