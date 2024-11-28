6a2b2e469c2543316fc941dc728c90c705ced705
VEMULA SHANATH KUMAR <65351126+shanathvemula@users.noreply.github.com>
2024-11-29 02:05:20 +0530

    Updated code

--- playDate/settings.py
+++ playDate/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Load the .env file
 env_path = os.path.join(BASE_DIR, ".env")
 load_dotenv(dotenv_path=env_path)
-GDAL_LIBRARY_PATH = os.getenv('GDAL_LIBRARY_PATH')
+# GDAL_LIBRARY_PATH = os.getenv('GDAL_LIBRARY_PATH')
 fernet = Fernet(os.getenv('fernet_key').encode('utf-8'))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
