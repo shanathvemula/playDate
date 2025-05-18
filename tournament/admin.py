b861b6420874a3678d52538bd81ade57cd731d07
VEMULA SHANATH KUMAR <65351126+shanathvemula@users.noreply.github.com>
2025-05-18 11:36:13 +0530

    updated admin

--- tournament/admin.py
+++ tournament/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from tournament.models import Tournament
+from tournament.models import Tournament, Teams, MatchScore
 
 admin.site.register(Tournament)
+admin.site.register(Teams)
+admin.site.register(MatchScore)
---
+++ tournament/admin.py~
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+# Register your models here.
+from tournament.models import Tournament
+
+admin.site.register(Tournament)
