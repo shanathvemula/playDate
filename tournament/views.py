f8c439557e02da29f31fd2d1dc98d671dc8dadd6
VEMULA SHANATH KUMAR <65351126+shanathvemula@users.noreply.github.com>
2025-06-06 03:24:02 +0530

    Updated team creation, update with user mail

--- tournament/views.py
+++ tournament/views.py
@@ -27,10 +27,13 @@ from tournament.serializers import (TournamentSerializer, TournamentSerializerDe
                                     TeamsSerializer, TeamsSerializerDepth, IdSerializer)
 from tournament.models import Tournament, Teams, GroundNew
 from payments.models import Transaction
+
+from playDate.settings import BASE_DIR
 from app.models import User
+from app.views import send_mail
 
 from django.db import connection
-
+import os
 
 # Create your views here.
 
@@ -256,6 +259,33 @@ class TeamsCRUD(APIView):
             data = request.data
             serializer = TeamsSerializer(data=data)
             if serializer.is_valid():
+                emails = [x['email'] for x in data['team']]
+                exists_emails = [x.email for x in User.objects.filter(email__in=emails)]
+                new_emails = [team for team in data['team'] if team['email'] not in exists_emails]
+                owner = User.objects.get(id=data['owner'])
+                # print(new_emails)
+                for team in new_emails:
+                    user = User.objects.create_user(username=team['email'],
+                                                    email=team['email'],
+                                                    password='Test@123',
+                                                    user_type='End User')
+                    with open(os.path.join(BASE_DIR / "templates" / "mail_templates" / "Team_invite_template.html"),
+                              "r") as html:
+                        body = html.read()
+                    # logo_path = os.path.join( "media", "logo", "Logo_without_background.png")
+                    # logInURL = os.getenv("logInURL")
+                    # print(os.getenv('logoURL'))
+                    body = (body.replace("{{ logo_path }}", os.getenv('logoURL')).replace("{{ app_name }}",
+                                                                                          os.getenv('app_name'))
+                            .replace("{{ first_name }}", team['name']).replace("{{ email }}", team['email'])
+                            .replace("{{ logInURL }}", os.getenv("logInURL")).replace("{{ current_year }}",
+                                                                                      str(datetime.now().year))
+                            .replace("{{ company_address }}", os.getenv("company_address"))
+                            .replace("{{ supportMail }}", os.getenv("supportMail"))
+                            .replace("{{ password }}", 'Test@123').replace("{{ owner }}", owner.first_name)
+                            .replace("{{ team_name }}", data['name']))
+                    send_mail(to=team['email'],
+                              subject=f"Welcome to {os.getenv('app_name')}! Thank you for registering", body=body)
                 serializer.save()
                 return HttpResponse(JSONRenderer().render(serializer.data), content_type='application/json',
                                     status=status.HTTP_201_CREATED)
@@ -273,6 +303,33 @@ class TeamsCRUD(APIView):
             team = Teams.objects.get(id=data['id'])
             serializer = TeamsSerializer(team, data=data, partial=True)
             if serializer.is_valid():
+                emails = [x['email'] for x in data['team']]
+                exists_emails = [x.email for x in User.objects.filter(email__in=emails)]
+                new_emails = [team for team in data['team'] if team['email'] not in exists_emails]
+                owner = User.objects.get(id=data['owner'])
+                # print(new_emails)
+                for team in new_emails:
+                    user = User.objects.create_user(username=team['email'],
+                                                    email=team['email'],
+                                                    password='Test@123',
+                                                    user_type='End User')
+                    with open(os.path.join(BASE_DIR / "templates" / "mail_templates" / "Team_invite_template.html"),
+                              "r") as html:
+                        body = html.read()
+                    # logo_path = os.path.join( "media", "logo", "Logo_without_background.png")
+                    # logInURL = os.getenv("logInURL")
+                    # print(os.getenv('logoURL'))
+                    body = (body.replace("{{ logo_path }}", os.getenv('logoURL')).replace("{{ app_name }}",
+                                                                                          os.getenv('app_name'))
+                            .replace("{{ first_name }}", team['name']).replace("{{ email }}", team['email'])
+                            .replace("{{ logInURL }}", os.getenv("logInURL")).replace("{{ current_year }}",
+                                                                                      str(datetime.now().year))
+                            .replace("{{ company_address }}", os.getenv("company_address"))
+                            .replace("{{ supportMail }}", os.getenv("supportMail"))
+                            .replace("{{ password }}", 'Test@123').replace("{{ owner }}", owner.first_name)
+                            .replace("{{ team_name }}", data['name']))
+                    send_mail(to=team['email'],
+                              subject=f"Welcome to {os.getenv('app_name')}! Thank you for registering", body=body)
                 serializer.save()
                 return HttpResponse(JSONRenderer().render(serializer.data), content_type='application/json')
             return HttpResponse(JSONRenderer().render(serializer.errors), status=status.HTTP_400_BAD_REQUEST)
